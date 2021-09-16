fac2f2272b082cf7f00126842893a84ab84464ec
Esther Wolfs:git config --global user.name Esther <esther.wolfs@student.fontys.nl>
2021-09-16 13:10:14 +0200

    Added Product class and ProductType enum

---
+++ MediaBazaar/MediaBazaar/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaBazaar
+{
+    class Product
+    {
+        private static int productId;
+        private int barcode;
+        private string name;
+        private ProductType type;
+        private int amount;
+
+        // properties
+        public static int ProductId { get; set; }
+
+        // constructor
+        public Product()
+        {
+
+        }
+        // methods
+    }
+}
