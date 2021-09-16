d058660e61b7320e1f80632313464b17dc37c213
Esther Wolfs:git config --global user.name Esther <esther.wolfs@student.fontys.nl>
2021-09-16 15:06:20 +0200

    Update sales employee form

--- MediaBazaar/MediaBazaar/FormSalesEmployee.Designer.cs
+++ MediaBazaar/MediaBazaar/FormSalesEmployee.Designer.cs
@@ -33,13 +33,24 @@ namespace MediaBazaar
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.btnLogout = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.lbxProducts = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbxAmount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRequestReplenishment = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Location = new System.Drawing.Point(13, 32);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -58,12 +69,16 @@ namespace MediaBazaar
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.lbxProducts);
             this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(242, 92);
+            this.tabPage2.Size = new System.Drawing.Size(971, 472);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Products ";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // btnLogout
@@ -75,6 +90,81 @@ namespace MediaBazaar
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = true;
             // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(971, 472);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // lbxProducts
+            // 
+            this.lbxProducts.FormattingEnabled = true;
+            this.lbxProducts.ItemHeight = 20;
+            this.lbxProducts.Location = new System.Drawing.Point(32, 52);
+            this.lbxProducts.Name = "lbxProducts";
+            this.lbxProducts.Size = new System.Drawing.Size(398, 404);
+            this.lbxProducts.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "List of products:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(757, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "<<Department>>";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRequestReplenishment);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbxAmount);
+            this.groupBox1.Location = new System.Drawing.Point(436, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 125);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Replenishment";
+            // 
+            // tbxAmount
+            // 
+            this.tbxAmount.Location = new System.Drawing.Point(96, 46);
+            this.tbxAmount.Name = "tbxAmount";
+            this.tbxAmount.Size = new System.Drawing.Size(125, 27);
+            this.tbxAmount.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Amount:";
+            // 
+            // btnRequestReplenishment
+            // 
+            this.btnRequestReplenishment.Location = new System.Drawing.Point(28, 79);
+            this.btnRequestReplenishment.Name = "btnRequestReplenishment";
+            this.btnRequestReplenishment.Size = new System.Drawing.Size(193, 29);
+            this.btnRequestReplenishment.TabIndex = 2;
+            this.btnRequestReplenishment.Text = "Request replenishment";
+            this.btnRequestReplenishment.UseVisualStyleBackColor = true;
+            // 
             // FormSalesEmployee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -85,6 +175,10 @@ namespace MediaBazaar
             this.Name = "FormSalesEmployee";
             this.Text = "FormSalesEmployee";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -95,5 +189,13 @@ namespace MediaBazaar
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRequestReplenishment;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxAmount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbxProducts;
     }
 }
