d21ed2f4c0f47530168cfab2d08f32b28e67fb58
Khan,Mohammad Nazibul M.N.K <nazibulkabir.khan@student.fontys.nl>
2021-09-16 12:47:28 +0200

    Initializing Form

--- MediaBazaar/MediaBazaar/Form1.Designer.cs
+++ MediaBazaar/MediaBazaar/Form1.Designer.cs
@@ -29,13 +29,335 @@ namespace MediaBazaar
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lstOverviewEmployee = new System.Windows.Forms.ListBox();
+            this.lstOverviewProduct = new System.Windows.Forms.ListBox();
+            this.lstOverviewAnnoucement = new System.Windows.Forms.ListBox();
+            this.lstOverviewComplaint = new System.Windows.Forms.ListBox();
+            this.lstOverviewSchedule = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lstEmployees = new System.Windows.Forms.ListBox();
+            this.btnRemoveEmployee = new System.Windows.Forms.Button();
+            this.btnAddNewEmployee = new System.Windows.Forms.Button();
+            this.btnEditEmployee = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Employee = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Day = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tabControl1.Location = new System.Drawing.Point(36, 30);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1120, 538);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.lstOverviewSchedule);
+            this.tabPage1.Controls.Add(this.lstOverviewComplaint);
+            this.tabPage1.Controls.Add(this.lstOverviewAnnoucement);
+            this.tabPage1.Controls.Add(this.lstOverviewProduct);
+            this.tabPage1.Controls.Add(this.lstOverviewEmployee);
+            this.tabPage1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tabPage1.Location = new System.Drawing.Point(4, 30);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1112, 504);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Overview";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Controls.Add(this.checkBox3);
+            this.tabPage2.Controls.Add(this.checkBox2);
+            this.tabPage2.Controls.Add(this.checkBox1);
+            this.tabPage2.Controls.Add(this.monthCalendar1);
+            this.tabPage2.Controls.Add(this.btnEditEmployee);
+            this.tabPage2.Controls.Add(this.btnAddNewEmployee);
+            this.tabPage2.Controls.Add(this.btnRemoveEmployee);
+            this.tabPage2.Controls.Add(this.lstEmployees);
+            this.tabPage2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tabPage2.Location = new System.Drawing.Point(4, 30);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1112, 504);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Employees";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lstOverviewEmployee
+            // 
+            this.lstOverviewEmployee.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstOverviewEmployee.FormattingEnabled = true;
+            this.lstOverviewEmployee.ItemHeight = 17;
+            this.lstOverviewEmployee.Location = new System.Drawing.Point(6, 38);
+            this.lstOverviewEmployee.Name = "lstOverviewEmployee";
+            this.lstOverviewEmployee.Size = new System.Drawing.Size(218, 429);
+            this.lstOverviewEmployee.TabIndex = 0;
+            // 
+            // lstOverviewProduct
+            // 
+            this.lstOverviewProduct.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstOverviewProduct.FormattingEnabled = true;
+            this.lstOverviewProduct.ItemHeight = 17;
+            this.lstOverviewProduct.Location = new System.Drawing.Point(888, 38);
+            this.lstOverviewProduct.Name = "lstOverviewProduct";
+            this.lstOverviewProduct.Size = new System.Drawing.Size(218, 429);
+            this.lstOverviewProduct.TabIndex = 1;
+            // 
+            // lstOverviewAnnoucement
+            // 
+            this.lstOverviewAnnoucement.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstOverviewAnnoucement.FormattingEnabled = true;
+            this.lstOverviewAnnoucement.ItemHeight = 17;
+            this.lstOverviewAnnoucement.Location = new System.Drawing.Point(242, 38);
+            this.lstOverviewAnnoucement.Name = "lstOverviewAnnoucement";
+            this.lstOverviewAnnoucement.Size = new System.Drawing.Size(313, 123);
+            this.lstOverviewAnnoucement.TabIndex = 2;
+            // 
+            // lstOverviewComplaint
+            // 
+            this.lstOverviewComplaint.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstOverviewComplaint.FormattingEnabled = true;
+            this.lstOverviewComplaint.ItemHeight = 17;
+            this.lstOverviewComplaint.Location = new System.Drawing.Point(561, 38);
+            this.lstOverviewComplaint.Name = "lstOverviewComplaint";
+            this.lstOverviewComplaint.Size = new System.Drawing.Size(313, 123);
+            this.lstOverviewComplaint.TabIndex = 3;
+            // 
+            // lstOverviewSchedule
+            // 
+            this.lstOverviewSchedule.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstOverviewSchedule.FormattingEnabled = true;
+            this.lstOverviewSchedule.ItemHeight = 17;
+            this.lstOverviewSchedule.Location = new System.Drawing.Point(242, 218);
+            this.lstOverviewSchedule.Name = "lstOverviewSchedule";
+            this.lstOverviewSchedule.Size = new System.Drawing.Size(632, 242);
+            this.lstOverviewSchedule.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(6, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Employee List:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(888, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Product List:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(242, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Announcements:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(561, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Complaints";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(242, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Schedule:";
+            // 
+            // lstEmployees
+            // 
+            this.lstEmployees.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstEmployees.FormattingEnabled = true;
+            this.lstEmployees.ItemHeight = 17;
+            this.lstEmployees.Location = new System.Drawing.Point(22, 18);
+            this.lstEmployees.Name = "lstEmployees";
+            this.lstEmployees.Size = new System.Drawing.Size(285, 361);
+            this.lstEmployees.TabIndex = 1;
+            // 
+            // btnRemoveEmployee
+            // 
+            this.btnRemoveEmployee.Location = new System.Drawing.Point(135, 453);
+            this.btnRemoveEmployee.Name = "btnRemoveEmployee";
+            this.btnRemoveEmployee.Size = new System.Drawing.Size(172, 28);
+            this.btnRemoveEmployee.TabIndex = 2;
+            this.btnRemoveEmployee.Text = "Remove Employee";
+            this.btnRemoveEmployee.UseVisualStyleBackColor = true;
+            // 
+            // btnAddNewEmployee
+            // 
+            this.btnAddNewEmployee.Location = new System.Drawing.Point(135, 385);
+            this.btnAddNewEmployee.Name = "btnAddNewEmployee";
+            this.btnAddNewEmployee.Size = new System.Drawing.Size(172, 28);
+            this.btnAddNewEmployee.TabIndex = 3;
+            this.btnAddNewEmployee.Text = "Add New Employee";
+            this.btnAddNewEmployee.UseVisualStyleBackColor = true;
+            // 
+            // btnEditEmployee
+            // 
+            this.btnEditEmployee.Location = new System.Drawing.Point(135, 419);
+            this.btnEditEmployee.Name = "btnEditEmployee";
+            this.btnEditEmployee.Size = new System.Drawing.Size(172, 28);
+            this.btnEditEmployee.TabIndex = 4;
+            this.btnEditEmployee.Text = "Edit Employee Info";
+            this.btnEditEmployee.UseVisualStyleBackColor = true;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(343, 36);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(447, 241);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(102, 25);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(447, 210);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(102, 25);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(447, 272);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(102, 25);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Employee,
+            this.Day,
+            this.Time});
+            this.dataGridView1.Location = new System.Drawing.Point(594, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(344, 238);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // Employee
+            // 
+            this.Employee.HeaderText = "Employee";
+            this.Employee.Name = "Employee";
+            // 
+            // Day
+            // 
+            this.Day.HeaderText = "Day";
+            this.Day.Name = "Day";
+            // 
+            // Time
+            // 
+            this.Time.HeaderText = "Time";
+            this.Time.Name = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1292, 580);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstOverviewSchedule;
+        private System.Windows.Forms.ListBox lstOverviewComplaint;
+        private System.Windows.Forms.ListBox lstOverviewAnnoucement;
+        private System.Windows.Forms.ListBox lstOverviewProduct;
+        private System.Windows.Forms.ListBox lstOverviewEmployee;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Employee;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Day;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button btnEditEmployee;
+        private System.Windows.Forms.Button btnAddNewEmployee;
+        private System.Windows.Forms.Button btnRemoveEmployee;
+        private System.Windows.Forms.ListBox lstEmployees;
     }
 }
 
