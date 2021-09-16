b9cfa3488951d8195c0683524ac4141f3ad3f15e
Khan,Mohammad Nazibul M.N.K <nazibulkabir.khan@student.fontys.nl>
2021-09-16 15:20:36 +0200

    Form

---
+++ MediaBazaar/MediaBazaar/Class/Empolyee.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaBazaar
+    {
+        abstract class Employee
+        {
+
+            // fields
+
+            private int employeeID;
+            private string lastname;
+            private string firstname;
+            private string gender;
+            private int phonenumber;
+            private string email;
+            private string address;
+            private DateTime dateofbirth;
+            private int bsn;
+            private string username;
+            private string password;
+
+            // properties
+
+            public int EmployeeID { get; private set; }
+
+
+            // constructors
+
+            public Employee(int employeeID, string lastname, string firstname, string gender, int phonenumber, string email, string address, DateTime dateofbirth, int bsn, string username, string password)
+            {
+                this.employeeID = employeeID;
+                this.lastname = lastname;
+                this.firstname = firstname;
+                this.gender = gender;
+                this.phonenumber = phonenumber;
+                this.email = email;
+                this.address = address;
+                this.dateofbirth = dateofbirth;
+                this.bsn = bsn;
+                this.username = username;
+                this.password = password;
+            }
+
+            public Employee(string lastname, string firstname, int bsn, string username, string password)
+            {
+                this.lastname = lastname;
+                this.firstname = firstname;
+                this.bsn = bsn;
+                this.username = username;
+                this.password = password;
+            }
+
+
+
+
+
+
+        }
+    }
--- MediaBazaar/MediaBazaar/Form/FormSalesManager.Designer.cs
+++ MediaBazaar/MediaBazaar/Form/FormSalesManager.Designer.cs
@@ -33,58 +33,108 @@ namespace MediaBazaar
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.btnLogout = new System.Windows.Forms.Button();
+            this.btnRequest = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAmountRequested = new System.Windows.Forms.TextBox();
+            this.lstProductSales = new System.Windows.Forms.ListBox();
             this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(13, 38);
+            this.tabControl1.Location = new System.Drawing.Point(11, 28);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(962, 498);
+            this.tabControl1.Size = new System.Drawing.Size(842, 374);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(954, 465);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Size = new System.Drawing.Size(834, 346);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Controls.Add(this.btnRequest);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.txtAmountRequested);
+            this.tabPage2.Controls.Add(this.lstProductSales);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(242, 92);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage2.Size = new System.Drawing.Size(834, 346);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // btnLogout
             // 
-            this.btnLogout.Location = new System.Drawing.Point(877, 12);
+            this.btnLogout.Location = new System.Drawing.Point(767, 9);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(94, 29);
+            this.btnLogout.Size = new System.Drawing.Size(82, 22);
             this.btnLogout.TabIndex = 1;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = true;
             // 
+            // btnRequest
+            // 
+            this.btnRequest.Location = new System.Drawing.Point(358, 67);
+            this.btnRequest.Name = "btnRequest";
+            this.btnRequest.Size = new System.Drawing.Size(75, 39);
+            this.btnRequest.TabIndex = 7;
+            this.btnRequest.Text = "Request";
+            this.btnRequest.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(360, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Amount:";
+            // 
+            // txtAmountRequested
+            // 
+            this.txtAmountRequested.Location = new System.Drawing.Point(360, 38);
+            this.txtAmountRequested.Name = "txtAmountRequested";
+            this.txtAmountRequested.Size = new System.Drawing.Size(56, 23);
+            this.txtAmountRequested.TabIndex = 5;
+            // 
+            // lstProductSales
+            // 
+            this.lstProductSales.FormattingEnabled = true;
+            this.lstProductSales.ItemHeight = 15;
+            this.lstProductSales.Location = new System.Drawing.Point(21, 20);
+            this.lstProductSales.Name = "lstProductSales";
+            this.lstProductSales.Size = new System.Drawing.Size(331, 304);
+            this.lstProductSales.TabIndex = 4;
+            // 
             // FormSalesManager
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(987, 548);
+            this.ClientSize = new System.Drawing.Size(864, 411);
             this.Controls.Add(this.btnLogout);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormSalesManager";
             this.Text = "FormSalesManager";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -95,5 +145,9 @@ namespace MediaBazaar
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnRequest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAmountRequested;
+        private System.Windows.Forms.ListBox lstProductSales;
     }
 }
