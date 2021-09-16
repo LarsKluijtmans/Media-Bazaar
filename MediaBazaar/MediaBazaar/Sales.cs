aa359e509a2ea3c10a3a04fdfa3b25cbff151480
Kluijtmans,Lars L <l.kluijtmans@student.fontys.nl>
2021-09-16 13:34:15 +0200

    classes: Sales Depot Office Department

---
+++ MediaBazaar/MediaBazaar/Sales.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaBazaar
+{
+    class Sales: Departments
+    {
+        private int Floor;
+
+        public Sales(int floor, int DeprtmentID, string DepartmentName) : base(DeprtmentID, DepartmentName) 
+        {
+            Floor = floor;
+        }
+
+        public void RequestProduct( /*Product p ,*/ int Amount)
+        {
+            /* Send product and amount to the Depot department*/ 
+        }
+    }
+}
