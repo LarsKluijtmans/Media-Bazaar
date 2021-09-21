fbcb3056dabb048e6fa911d36d5b161b6682356e
Esther Wolfs:git config --global user.name Esther <esther.wolfs@student.fontys.nl>
2021-09-21 10:04:04 +0200

    fix error

--- MediaBazaar/MediaBazaar/Form/FormSalesEmployee.Designer.cs
+++ MediaBazaar/MediaBazaar/Form/FormSalesEmployee.Designer.cs
@@ -106,7 +106,6 @@ namespace MediaBazaar
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Profile";
             this.tabPage3.UseVisualStyleBackColor = true;
-            this.tabPage3.Click += new System.EventHandler(this.tabPage3_Click);
             // 
             // lbxProducts
             // 
