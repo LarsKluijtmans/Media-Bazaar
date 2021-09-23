4c1bf595c9a81f01cb3270713f03c40bb4fddf50
Esther Wolfs:git config --global user.name Esther <esther.wolfs@student.fontys.nl>
2021-09-23 14:35:33 +0200

    changes

--- MediaBazaar/MediaBazaar/Form/FormLogin.cs
+++ MediaBazaar/MediaBazaar/Form/FormLogin.cs
@@ -13,7 +13,6 @@ namespace MediaBazaar
             InitializeComponent();
             mediaBazaar = new Store();
 
-            TestEmployees();
             
 
             Employee e = new Employee("Wolfs1", "Esther1", "F", 0612901749, "ewolfs@mb.com", "drunen", "01-01-1998", 1234, "esther", "wolfs", JobTitle.DEPOT_MANAGER);
@@ -46,19 +45,6 @@ namespace MediaBazaar
                     MessageBox.Show("Wrong username/password");
                 }
             }
-        }
-        public void TestEmployees()
-        {
-            mediaBazaar.AddEmployee("Wolfs", "Esther", "F", "ewolfs@mb.com", 0612901749, "Drunen", "01-01-1998", 1234, "esther", "wolfs", JobTitle.SALES_MANAGER);
-            mediaBazaar.AddEmployee("2", "Employee", "M", "employee2@mb.com", 0643810212, "Eindhoven", "01-01-1998", 1234, "employee", "2", JobTitle.DEPOT_MANAGER);
-            mediaBazaar.AddEmployee("3", "Employee", "X", "employee3@mb.com", 0689313810, "Den Bosch", "01-01-1998", 1234, "employee", "3", JobTitle.DEPOT_EMPLOYEE);
-            mediaBazaar.AddEmployee("4", "Employee", "F", "employee4@mb.com", 0648936401, "Breda", "01-01-1998", 1234, "employee", "4", JobTitle.SALES_EMPLOYEE);
-            mediaBazaar.AddEmployee("5", "Employee", "M", "employee5@mb.com", 0613861920, "Tilburg", "01-01-1998", 1234, "employee", "5", JobTitle.SALES_EMPLOYEE);
-            mediaBazaar.AddEmployee("6", "Employee", "X", "employee6@mb.com", 0645739103, "Amsterdam", "01-01-1998", 1234, "employee", "6", JobTitle.SALES_MANAGER);
-            mediaBazaar.AddEmployee("7", "Employee", "F", "employee7@mb.com", 0612345678, "Eindhoven", "01-01-1998", 1234, "employee", "7", JobTitle.OFFICE_EMPLOYEE);
-            mediaBazaar.AddEmployee("8", "Employee", "F", "employee8@mb.com", 0647581639, "Eindhoven", "01-01-1998", 1234, "employee", "8", JobTitle.OFFICE_MANAGER);
-            mediaBazaar.AddEmployee("9", "Employee", "M", "employee9@mb.com", 0658690364, "Amsterdam", "01-01-1998", 1234, "employee", "9", JobTitle.SALES_EMPLOYEE);
-            mediaBazaar.AddEmployee("10", "Employee", "F", "employee10@mb.com", 0646123749, "Eindhoven", "01-01-1998", 1234, "employee", "10", JobTitle.SALES_EMPLOYEE);
         }
         private void UpdateListbox()
         {
