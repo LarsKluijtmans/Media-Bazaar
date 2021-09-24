b239ddce07fb542a340b7a9f27c2e1047fdabd2c
Kluijtmans,Lars L <l.kluijtmans@student.fontys.nl>
2021-09-24 20:37:58 +0200

    --

---
+++ MediaBazaar/MediaBazaar/Class/ProductsForDepot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaBazaar.Class
+{
+    class ProductsForDepot
+    {
+        /*add new product*/
+        /*find and remove a product*/
+        /*request for stock*/
+        /* fufill shelf replenish reuests*/
+
+    }
+}
