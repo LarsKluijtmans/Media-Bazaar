e5ab0655cf5f7c491681b66dc8b2dc3eb352c02f
Soleil Umwiza <i474883@fhict.nl>
2021-09-24 07:50:30 +0200

    New product added

--- MediaBazaar/MediaBazaar/Form/FormNewEmployee.cs
+++ MediaBazaar/MediaBazaar/Form/FormNewEmployee.cs
@@ -23,17 +23,17 @@ namespace MediaBazaar
 
         private void BtnNewEmployee_Click(object sender, EventArgs e)
         {
-            string firstN = txtFirstName.Text;
-            string lastN = txtLastN.Text;
-            int phoneN = Convert.ToInt32(txtPhoneN.Text);
-            int bsn = Convert.ToInt32(txtBsn.Text);
-            string username = txtUserN.Text;
-            string email = txtEmail.Text;
-            string password = txtPsswrd.Text;
+           // string firstN = txtFirstName.Text;
+            //string lastN = txtLastN.Text;
+            //int phoneN = Convert.ToInt32(txtPhoneN.Text);
+           // int bsn = Convert.ToInt32(txtBsn.Text);
+            //string username = txtUserN.Text;
+            //string email = txtEmail.Text;
+            //string password = txtPsswrd.Text;
 
-            Employee newEmp = new Employee(firstN, lastN, phoneN, bsn, username, password, email);
-            employee.Add(newEmp);
-            OpenForm1(firstN, lastN, phoneN, bsn, username, email, password);
+            //Employee newEmp = new Employee(firstN, lastN, phoneN, bsn, username, password, email);
+            //employee.Add(newEmp);
+            //OpenForm1(firstN, lastN, phoneN, bsn, username, email, password);
 
         }
 
@@ -43,5 +43,7 @@ namespace MediaBazaar
             newEmployee.Show();
            
         }
+
+        
     }
 }
