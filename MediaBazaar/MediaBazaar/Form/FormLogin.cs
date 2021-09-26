cc959e47b60e18940e3265a045b4b3ad645c3afe
Kluijtmans,Lars L <l.kluijtmans@student.fontys.nl>
2021-09-26 15:53:52 +0200

    Login made

--- MediaBazaar/MediaBazaar/Form/FormLogin.cs
+++ MediaBazaar/MediaBazaar/Form/FormLogin.cs
@@ -1,32 +1,121 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using MediaBazaar.Class;
+using System;
 using System.Windows.Forms;
 
 namespace MediaBazaar
 {
     public partial class FormLogin : Form
     {
-        Store mediaBazaar;
-        Employee employee;
+        int Userid;
 
         public FormLogin()
         {
             InitializeComponent();
-            mediaBazaar = new Store();
-
-            
-
-            employee = new Employee("Wolfs1", "Esther1",  0612901749, "ewolfs@mb.com", "drunen", "01-01-1998", 1234, "esther", "wolfs", JobTitle.DEPOT_MANAGER);
-            mediaBazaar.Employees.Add(employee);
-
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            string username = tbxUsername.Text;
-            string password = tbxPassword.Text;
+            MySqlConnection conn = Utils.GetConnection();
+            string sql = Utils.LOGIN_BY_EMPLOYEEID;
+            try
+            {
+                string ID = tbxUsername.Text;
+                if (string.IsNullOrEmpty(ID))
+                {
+                    MessageBox.Show("'id' field is required.");
+                    return;
+                }
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
 
-            FormSalesManager formSalesManager = new FormSalesManager(employee, mediaBazaar);
-            formSalesManager.Show();
+                cmd.Parameters.AddWithValue("@employeeid", ID);
+
+                conn.Open();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    if (Convert.ToInt32(reader[0]) == Convert.ToInt32(tbxUsername.Text))
+                    {
+                        if (reader[1].ToString() == tbxPassword.Text)
+                        {
+                            
+
+                            switch (reader[2].ToString())
+                            {
+                                case "OWNER": Form1 form1 = new Form1(mediaBazaar.GetEmployee(eee.EmployeeID), mediaBazaar); form1.Show(); break;
+                                case "SALES_MANAGER": FormSalesManager formSalesManager = new FormSalesManager(mediaBazaar.GetEmployee(eee.EmployeeID), mediaBazaar); formSalesManager.Show(); break;
+                                case "SALES_EMPLOYEE": FormSalesEmployee formSalesEmployee = new FormSalesEmployee(mediaBazaar.GetEmployee(eee.EmployeeID), mediaBazaar); formSalesEmployee.Show(); break;
+                                case "OFFICE_MANAGER": FormOfficeManager formOfficeManager = new FormOfficeManager(mediaBazaar.GetEmployee(eee.EmployeeID), mediaBazaar); formOfficeManager.Show(); break;
+                                case "OFFICE_EMPLOYE": FormOfficeEmployee formOfficeEmployee = new FormOfficeEmployee(mediaBazaar.GetEmployee(eee.EmployeeID), mediaBazaar); formOfficeEmployee.Show(); break;
+                                case "DEPOT_MANAGER": FormDepotManager formDepotManager = new FormDepotManager(mediaBazaar.GetEmployee(eee.EmployeeID), mediaBazaar); formDepotManager.Show(); break;
+                                case "DEPOT_EMPLOYEE": FormDepotEmployee formDepotEmployee = new FormDepotEmployee(mediaBazaar.GetEmployee(eee.EmployeeID), mediaBazaar); formDepotEmployee.Show(); break;
+                            }
+
+
+                            break;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Make sure t put in the right password.");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Make sure t put in the right ID number.");
+                        break;
+                    }
+                }
+            }
+            catch (MySqlException msqEx)
+            {
+                MessageBox.Show(msqEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //string username = tbxUsername.Text;
+            //string password = tbxPassword.Text;
+
+            //FormSalesManager formSalesManager = new FormSalesManager(employee, mediaBazaar);
+            //formSalesManager.Show();
 
             //foreach (Employee eee in mediaBazaar.Employees)
             //{
