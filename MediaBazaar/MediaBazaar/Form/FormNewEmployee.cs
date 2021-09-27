aef0142e6a7a5fbadecc8606e6bd7644a976307c
Esther Wolfs:git config --global user.name Esther <esther.wolfs@student.fontys.nl>
2021-09-27 13:28:54 +0200

    update contract class

--- MediaBazaar/MediaBazaar/Form/FormNewEmployee.cs
+++ MediaBazaar/MediaBazaar/Form/FormNewEmployee.cs
@@ -10,38 +10,14 @@ namespace MediaBazaar
 {
     public partial class FormNewEmployee : Form
     {
-        List<Employee> employee;
-        
-
         public FormNewEmployee()
         {
             InitializeComponent();
-            employee = new List<Employee>();
-         
-
         }
 
         private void BtnNewEmployee_Click(object sender, EventArgs e)
         {
-           // string firstN = txtFirstName.Text;
-            //string lastN = txtLastN.Text;
-            //int phoneN = Convert.ToInt32(txtPhoneN.Text);
-           // int bsn = Convert.ToInt32(txtBsn.Text);
-            //string username = txtUserN.Text;
-            //string email = txtEmail.Text;
-            //string password = txtPsswrd.Text;
 
-            //Employee newEmp = new Employee(firstN, lastN, phoneN, bsn, username, password, email);
-            //employee.Add(newEmp);
-            //OpenForm1(firstN, lastN, phoneN, bsn, username, email, password);
-
-        }
-
-        public void OpenForm1(string firstN, string lastN, int phoneN, int bsn, string username, string email, string password)
-        {
-            Form1 newEmployee = new Form1(firstN, lastN, phoneN, bsn, username, email, password);
-            newEmployee.Show();
-           
         }
     }
 }
