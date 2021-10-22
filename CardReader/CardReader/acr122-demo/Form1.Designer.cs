3ee7f7407365748247e79fed6cc38997b678672f
Kluijtmans,Lars L <l.kluijtmans@student.fontys.nl>
2021-10-22 19:25:15 +0200

    Splitt the card reader and the connect card to employee apps

---
+++ CardReader/CardReader/acr122-demo/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace acr122_demo {
+    partial class Checks {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Checks));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbCheck = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbCheck
+            // 
+            this.lbCheck.FormattingEnabled = true;
+            this.lbCheck.Location = new System.Drawing.Point(18, 15);
+            this.lbCheck.Name = "lbCheck";
+            this.lbCheck.Size = new System.Drawing.Size(310, 173);
+            this.lbCheck.TabIndex = 0;
+            // 
+            // Checks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 213);
+            this.Controls.Add(this.lbCheck);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Checks";
+            this.Text = "Card Reader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox lbCheck;
+    }
+}
+
