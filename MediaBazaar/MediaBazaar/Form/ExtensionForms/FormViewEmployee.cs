409eb49c108869c144e0d29db42a53315889228b
Esther Wolfs:git config --global user.name Esther <esther.wolfs@student.fontys.nl>
2021-11-01 20:03:37 +0100

    fix employee/contract

--- MediaBazaar/MediaBazaar/Form/ExtensionForms/FormViewEmployee.cs
+++ MediaBazaar/MediaBazaar/Form/ExtensionForms/FormViewEmployee.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Windows.Forms;
 using ClassLibraryProject.Class;
+using ClassLibraryProject.ManagmentClasses;
 using MySql.Data.MySqlClient;
 
 
@@ -11,13 +12,11 @@ namespace MediaBazaar
     {
         Person p;
         Contract c;
-        int saveCount;
         public FormViewEmployee(Person p, Contract c)
         {
             InitializeComponent();
             this.p = p;
             this.c = c;
-            saveCount = 0;
 
             try
             {
@@ -49,14 +48,37 @@ namespace MediaBazaar
 
         private void btnEditData_Click(object sender, EventArgs e)
         {
+            UpdateEmployee();
+            UpdateContract();
+
+            this.Close();
+        }
+        public void UpdateEmployee()
+        {
             string employeeID = tbxEmployeeID.Text;
             string firstName = tbxFirstName.Text;
+            if (string.IsNullOrEmpty(firstName))
+            {
+                MessageBox.Show("Please enter a first name");
+            }
             string lastName = tbxLastName.Text;
+            if (string.IsNullOrEmpty(lastName))
+            {
+                MessageBox.Show("Please enter a last name");
+            }
             string city = tbxCity.Text;
+            if (string.IsNullOrEmpty(city))
+            {
+                MessageBox.Show("Please enter a city");
+            }
             string phoneNumber = tbxPhoneNumber.Text;
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+                MessageBox.Show("Please enter a phone number");
+            }
 
             MySqlConnection conn = Utils.GetConnection();
-            string sql = "Utils.UPDATE_EMPLOYEE";
+            string sql = EmployeeManagement.UPDATE_EMPLOYEE;
             try
             {
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
@@ -81,17 +103,50 @@ namespace MediaBazaar
             {
                 conn.Close();
             }
-
-            saveCount++;
-
-            this.Close();
         }
-        protected override void OnClosing(CancelEventArgs e)
+        public void UpdateContract()
         {
-            if (saveCount == 1)
+            string employeeID = tbxEmployeeID.Text;
+            string jobTitle = cbxJobTitle.SelectedItem.ToString();
+            if (string.IsNullOrEmpty(jobTitle))
             {
-                MessageBox.Show("Make sure to save the data");
-                saveCount = 0;
+                MessageBox.Show("Please select a job title");
+            }
+            string workHours = tbxWorkHours.Text;
+            if (string.IsNullOrEmpty(workHours))
+            {
+                MessageBox.Show("Please enter work hours");
+            }
+            string salary = tbxSalary.Text;
+            if (string.IsNullOrEmpty(salary))
+            {
+                MessageBox.Show("Please enter salary");
+            }
+
+            MySqlConnection conn = Utils.GetConnection();
+            string sql = ContractManagement.UPDATE_CONTRACT;
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@EmployeeID", employeeID);
+                cmd.Parameters.AddWithValue("@JobTitle", jobTitle);
+                cmd.Parameters.AddWithValue("@WorkHoursPerWeek", workHours);
+                cmd.Parameters.AddWithValue("@SalaryPerHour", salary);
+                conn.Open();
+
+                int numAffectedRows = cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException msqEx)
+            {
+                MessageBox.Show(msqEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong" + ex);
+            }
+            finally
+            {
+                conn.Close();
             }
         }
     }
