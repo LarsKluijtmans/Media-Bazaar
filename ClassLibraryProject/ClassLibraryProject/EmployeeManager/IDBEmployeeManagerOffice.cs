5fdce8e2e33d33ebb4210b742bf1c5f849424840
Esther Wolfs:git config --global user.name Esther <esther.wolfs@student.fontys.nl>
2022-01-09 14:52:06 +0100

    Update employee

--- ClassLibraryProject/ClassLibraryProject/EmployeeManager/IDBEmployeeManagerOffice.cs
+++ ClassLibraryProject/ClassLibraryProject/EmployeeManager/IDBEmployeeManagerOffice.cs
@@ -12,5 +12,6 @@ namespace ClassLibraryProject
         public bool UpdateEmployee(Employee e);
         public bool DeleteEmployee(Employee e);
         public Employee GetEmployeeID(string givenEmail, string jobTitle);
+        public Employee GetEmployeeByID(int givenEmployeeID);
     }
 }
