ccb4c2f2611ca0b0f2f854ce44dfbc629d09cfb7
Esther Wolfs:git config --global user.name Esther <esther.wolfs@student.fontys.nl>
2022-01-11 16:32:27 +0100

    changes to product class

--- ClassLibraryProject/ClassLibraryProject/Class/Product.cs
+++ ClassLibraryProject/ClassLibraryProject/Class/Product.cs
@@ -7,86 +7,55 @@ namespace ClassLibraryProject.Class
     {
         //fields
         private int productID;
+        private string productName;
         private string barcode;
-        private string name;
         private string productType;
-        private int amountInStore;
+        private int sellingPrice;
         private int amountInDepot;
+        private int amountInStore;
+        private bool isDiscontinued;
 
         private List<OrderInfo> orderInfos;
 
-        private int sellingPrice;
-        private bool isDiscontinued;
-
-
         //properties
-        public int ProductID
-        {
-            get { return productID; }
-            set { productID = value; }
-        }
-        public string Barcode
-        {
-            get { return barcode; }
-            set { barcode = value; }
-        }
-        public string ProductType
-        {
-            get { return productType; }
-            set { productType = value; }
-        }
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        public int AmountInStore
-        {
-            get { return amountInStore; }
-            set { amountInStore = value; }
-        }
-        public int AmountInDepot
-        {
-            get { return amountInDepot; }
-            set { amountInDepot = value; }
-        }
-        public List<OrderInfo> OrderInfos
-        {
-            get { return orderInfos; }
-            set { orderInfos = value; }
-        }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Barcode { get; set; }
+        public string ProductType { get; set; }
         public int SellingPrice { get; set; }
+        public int AmountInDepot { get; set; }
+        public int AmountInStore { get; set; }
         public bool IsDiscontinued { get; set; }
+
+        public List<OrderInfo> OrderInfos { get; set; }
+
         //constructor
-        public Product(int id, string name, string type, string barcode, int sellingPrice)
+        // for creating the product
+        public Product(string name, string type, string barcode)
         {
-            ProductID = id;
             Barcode = barcode;
-            Name = name;
+            ProductName = name;
             ProductType = type;
             AmountInDepot = 0;
             AmountInStore = 0;
-            SellingPrice = sellingPrice;
+            sellingPrice = 0;
             IsDiscontinued = false;
+
             OrderInfos = new List<OrderInfo>();
         }
-        public Product(int id, string name, string type, string barcode, int sellingPrice, int amountInDepot, int amountInStore)
+        // for reading the product
+        public Product(int id, string name, string type, string barcode, int sellingPrice, int amountInDepot, int amountInStore, bool isDiscontinued) 
         {
             ProductID = id;
             Barcode = barcode;
-            Name = name;
+            ProductName = name;
             ProductType = type;
             AmountInDepot = amountInDepot;
             AmountInStore = amountInStore;
             SellingPrice = sellingPrice;
-            IsDiscontinued = false;
+            IsDiscontinued = isDiscontinued;
+
             OrderInfos = new List<OrderInfo>();
-        }
-        // methods
-        public override string ToString()
-        {
-            return $"ID: {ProductID} - name:{Name}";
         }
     }
 }
