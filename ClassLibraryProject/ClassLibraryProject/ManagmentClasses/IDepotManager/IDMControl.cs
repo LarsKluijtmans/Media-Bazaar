4ee9bde0e59eac7d492642432223cc428c8ab607
Khan,Mohammad Nazibul M.N.K <nazibulkabir.khan@student.fontys.nl>
2022-01-13 01:54:16 +0100

    control

---
+++ ClassLibraryProject/ClassLibraryProject/ManagmentClasses/IDepotManager/IDMControl.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibraryProject.ManagmentClasses.IDepotManager
+{
+    interface IDMControl
+    {
+    }
+}
---
+++ ClassLibraryProject/ClassLibraryProject/ManagmentClasses/ISalesEmployee/ISEControl.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibraryProject.ManagmentClasses.ISalesEmployee
+{
+    interface ISEControl
+    {
+    }
+}
