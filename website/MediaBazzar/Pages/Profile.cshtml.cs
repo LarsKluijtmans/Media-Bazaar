5eb7432197ec5e6c6a4d9e6037a514fb801d4dc3
Kluijtmans,Lars L <l.kluijtmans@student.fontys.nl>
2022-01-16 15:07:40 +0100

    push change in website

--- website/MediaBazzar/Pages/Profile.cshtml.cs
+++ website/MediaBazzar/Pages/Profile.cshtml.cs
@@ -16,59 +16,31 @@ namespace MediaBazzar.Pages
     {
         [BindProperty]
         public Admin employee { get; set; }
-
+        DBEmployeeManager updateporfile = new DBEmployeeManager();
         public void OnGet()
         {
-            MySqlConnection conn = Utils.GetConnection();
+            updateporfile = new DBEmployeeManager();
+            Employee a = updateporfile.GetEmployeeByID();
 
-            string sql = EmployeeManagement.GET_EMPLOYEE_BY_USERNAME;
-
-            try
-            {
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-
-                cmd.Parameters.AddWithValue("@UserName", User.Identity.Name.ToString());
-
-                conn.Open();
-
-                MySqlDataReader reader = cmd.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    if (reader.GetInt32("Active") == 1)
-                    {
-                        employee = new Admin();
-
-                        employee.EmployeeID = reader.GetInt32("EmployeeID");
-                        employee.FirstName = reader.GetString("FirstName");
-                        employee.LastName = reader.GetString("LastName");
-                        employee.Username = reader.GetString("UserName");
-                        employee.Password = reader.GetString("Password");
-                        employee.BSN = reader.GetInt32("BSN");
-                        employee.City = reader.GetString("City");
-                        employee.Email = reader.GetString("Email");
-                        employee.PhoneNumber = reader.GetString("PhoneNumber");
-                    }
-                    else
-                    {
-                        ViewData["Message"] = "Whats worng whight you, get out of here!!!";
-                    }
-                }
-            }
-            catch (MySqlException)
-            {
-                ViewData["Message"] = "Error please try again later.";
-            }
-            finally
-            {
-                conn.Close();
-            }
+            employee.EmployeeID = a.EmployeeID;
+            employee.FirstName = a.FirstName;
+            employee.LastName = a.LastName;
+            employee.PhoneNumber = a.PhoneNumber;
+            employee.Email = a.Email;
+            employee.City = a.City;
+            employee.BSN = a.BSN;
+            employee.Username = a.Username;
+            employee.Password = a.Password;
+            employee.ZipCode = a.ZipCode;
+            employee.Address = a.Address;
+            employee.DateOfBirth = a.DateOfBirth;
+            employee.PersonalEmail = a.PersonalEmail;
         }
         public void OnPost()
         {
             if (ModelState.IsValid)
             {
-                DBEmployeeManager updateporfile = new DBEmployeeManager();
+                updateporfile = new DBEmployeeManager();
                 updateporfile.UpdateOwnInfo(employee);
             }
         }
