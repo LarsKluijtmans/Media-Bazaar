29fb73bff3aa2ed8b9b63da4b077864aea925e8a
Kluijtmans,Lars L <l.kluijtmans@student.fontys.nl>
2022-01-17 14:27:47 +0100

    Shedule in webite finished

--- website/MediaBazzar/Pages/Schedule.cshtml.cs
+++ website/MediaBazzar/Pages/Schedule.cshtml.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
+using System.Security.Claims;
+using ClassLibrary1;
 using ClassLibraryProject.Class;
+using ClassLibraryProject.dbClasses;
 using ClassLibraryProject.Enum;
 using ClassLibraryProject.ManagmentClasses;
 using Microsoft.AspNetCore.Authorization;
@@ -10,6 +13,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
+using Employee = ClassLibraryProject.Class.Employee;
 
 namespace MediaBazzar.Pages
 {
@@ -28,7 +32,7 @@ namespace MediaBazzar.Pages
         [BindProperty]
         public PreferedWorkTime schedule { get; set; }
         public List<PreferedWorkTime> pws { get; set; }
-       
+        public dbLoginManager dbLogin = new dbLoginManager();
         private CookieOptions cookieOptions = new CookieOptions
         {
             Expires = DateTime.Now.AddMinutes(5)
@@ -44,19 +48,19 @@ namespace MediaBazzar.Pages
 
         public void OnGet()
         {
+            //Geting curent date time
             DateTime time = DateTime.Now;
             DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
             if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
             {
                 time = time.AddDays(3);
             }
-
             week = CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
-
             year = time.Year;
-
             Day = Days.Monday;
             Shift = Shifts.Morning;
+
+            // on get
             pws = new List<PreferedWorkTime>();
             PlanningManagment planning = new PlanningManagment();
             List<PreferedWorkTime> schedules = planning.GetPlanningForEmployee(LoginModel.employeeID.ToString());
@@ -85,9 +89,13 @@ namespace MediaBazzar.Pages
 
         public WorkingToday IsWorkingToday()
         {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            Employee ee = dbLogin.GetEmployeeByEmail(userEmail);
+
             MySqlConnection conn = Utils.GetConnection();
 
-            string sql = $"SELECT shift FROM `planning` WHERE `Week` = {week} and `Year`= {year} and `Day` = '{Day}' and `EmployeeID` = {LoginModel.employeeID}";
+            string sql = $"SELECT shift FROM `planning` WHERE `Week` = {week} and `Year`= {year} and `Day` = '{Day}' and `EmployeeID` = {ee.EmployeeID}";
 
             try
             {
@@ -130,5 +138,8 @@ namespace MediaBazzar.Pages
 
             return WorkingToday;
         }
+
+        public void OnPost()
+        { }
     }
 }
