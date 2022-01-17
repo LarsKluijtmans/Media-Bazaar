870f5ed89eed341437fd404a35776997e3d6aeff
Esther Wolfs:git config --global user.name Esther <esther.wolfs@student.fontys.nl>
2022-01-17 16:59:23 +0100

    Enter availability

--- website/MediaBazzar/Models/DBAvailabilityManager.cs
+++ website/MediaBazzar/Models/DBAvailabilityManager.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ClassLibraryProject.Class;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +10,48 @@ namespace MediaBazaarWebsite
 {
     public class DBAvailabilityManager : IDBAvailabilityManager
     {
-        public bool EnterAvailability()
+        //sql
+        public string ENTER_AVAILABILITY = "INSERT INTO Availability (EmployeeID, Year, Week, Day) VALUES (@EmployeeID, @Year, @Week, @Day);";
+
+        public bool EnterAvailability(Unavailability u)
         {
-            throw new NotImplementedException();
+            MySqlConnection conn = Utils.GetConnection();
+            string sql = ENTER_AVAILABILITY;
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                conn.Open();
+
+                cmd.Parameters.AddWithValue("@EmployeeID", u.Employee.EmployeeID);
+                cmd.Parameters.AddWithValue("@Year", u.Year);
+                cmd.Parameters.AddWithValue("@Week", u.Week);
+                cmd.Parameters.AddWithValue("@Day", u.Day);
+
+                int numCreatedRows = cmd.ExecuteNonQuery();
+
+                if (numCreatedRows == 1)
+                {
+                    return true;
+                }
+            }
+            catch (MySqlException msqEx)
+            {
+                Debug.WriteLine(msqEx);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+
+            return false;
         }
     }
 }
