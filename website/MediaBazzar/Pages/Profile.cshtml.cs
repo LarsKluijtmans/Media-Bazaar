9ed1eeb8d676f8cdccd71907d6eb676846331b50
Soleil <i474883@fhict.nl>
2022-01-17 01:20:20 +0100

    Commit changes for getting employeeID on profile

--- website/MediaBazzar/Pages/Profile.cshtml.cs
+++ website/MediaBazzar/Pages/Profile.cshtml.cs
@@ -1,7 +1,5 @@
 using ClassLibraryProject.Class;
 using ClassLibraryProject.ManagmentClasses;
-using MySql.Data;
-using MySql.Data.MySqlClient;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -21,41 +19,41 @@ namespace MediaBazzar.Pages
         [BindProperty]
         public string Password { get; set; }
 
-        //public void OnGet()
-        //{
-        //    updateporfile = new DBEmployeeManager();
-        //    Employee a = updateporfile.GetEmployeeByID(LoginModel.emplContract.Employee.EmployeeID);
+        public void OnGet()
+        {
+            updateporfile = new DBEmployeeManager();
+            Employee a = updateporfile.GetEmployeeByID(LoginModel.employeeID);
 
-        //    employee.EmployeeID = a.EmployeeID;
-        //    employee.FirstName = a.FirstName;
-        //    employee.LastName = a.LastName;
-        //    employee.PhoneNumber = a.PhoneNumber;
-        //    employee.Email = a.Email;
-        //    employee.City = a.City;
-        //    employee.BSN = a.BSN;
-        //    employee.Username = a.Username;
-        //    employee.Password = a.Password;
-        //    employee.ZipCode = a.ZipCode;
-        //    employee.Address = a.Address;
-        //    employee.DateOfBirth = a.DateOfBirth;
-        //    employee.PersonalEmail = a.PersonalEmail;
-        //}
-        //public void OnPost()
-        //{
+            employee.EmployeeID = a.EmployeeID;
+            employee.FirstName = a.FirstName;
+            employee.LastName = a.LastName;
+            employee.PhoneNumber = a.PhoneNumber;
+            employee.Email = a.Email;
+            employee.City = a.City;
+            employee.BSN = a.BSN;
+            employee.Username = a.Username;
+            employee.Password = a.Password;
+            employee.ZipCode = a.ZipCode;
+            employee.Address = a.Address;
+            employee.DateOfBirth = a.DateOfBirth;
+            employee.PersonalEmail = a.PersonalEmail;
+        }
+        public void OnPost()
+        {
 
-        //    if (employee.Password == Password)
-        //    {
-        //        if (ModelState.IsValid)
-        //        {
-        //            updateporfile = new DBEmployeeManager();
-        //            updateporfile.UpdateOwnInfo(employee);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        ViewData["Message"] = "Passwords not the same.";
-        //    }
-        //}
+            if (employee.Password == Password)
+            {
+                if (ModelState.IsValid)
+                {
+                    updateporfile = new DBEmployeeManager();
+                    updateporfile.UpdateOwnInfo(employee);
+                }
+            }
+            else
+            {
+                ViewData["Message"] = "Passwords not the same.";
+            }
+        }
 
     }
 }
