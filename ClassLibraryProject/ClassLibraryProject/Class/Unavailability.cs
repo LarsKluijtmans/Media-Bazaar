f5492cd1620d263263fa85d255056307451f5731
Khan,Mohammad Nazibul M.N.K <nazibulkabir.khan@student.fontys.nl>
2022-01-17 16:33:10 +0100

    unavailability

---
+++ ClassLibraryProject/ClassLibraryProject/Class/Unavailability.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibraryProject.Class
+{
+    public class Unavailability
+    {
+        //fields
+        private int year;
+        private int week;
+        private string day;
+        private Employee employee;
+
+        //properties
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public string Day
+        {
+            get { return day; }
+            set { day = value; }
+        }
+        public int Week
+        {
+            get { return week; }
+            set { week = value; }
+        }
+        public Employee Employees
+        {
+            get { return employee; }
+            set { employee = value; }
+        }
+
+        //constructor
+        public Unavailability(int year, int week, string day, Employee employee)
+        {
+            Year = year;
+            Week = week;
+            Day = day;
+            Employees = employee;
+        }
+    }
+}
